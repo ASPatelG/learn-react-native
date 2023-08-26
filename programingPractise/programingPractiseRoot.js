dd7469f57af81d496f34488af3f732ede44aac80
AS-Patel <mca2020asp@gmail.com>
2023-08-26 18:27:00 +0530

    created 3 file to consume context data and skip prop dealing

--- programingPractise/programingPractiseRoot.js
+++ programingPractise/programingPractiseRoot.js
@@ -1,11 +1,17 @@
 import {useEffect} from 'react';
 import {Text, View, Pressable, StyleSheet} from 'react-native';
+
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 import {translationValues} from '../staticDataFiles/translationValues';
-const {hi:{ connectorApp, programingPractis}} = translationValues;
+const {hi:{ connectorApp, programingPractis, offlineCheck, checkContext, offlineSuccess}} = translationValues;
+import {crossPlatformToast} from '../components/crossPlatformToast';
 
 import { createTable } from '../offlineFunctionality/createTable';
 import { getData } from '../offlineFunctionality/getData';
 import { insertData } from '../offlineFunctionality/insertData';
+
+import {ContextDataConsume} from '../create_context/contextDataConsume';
 
 export const ProgramingPractiseRoot = (props)=>{
 	/* Used to show ui till the app is loading */
@@ -13,16 +19,20 @@ export const ProgramingPractiseRoot = (props)=>{
 	const {navigation} = props;
 
 	useEffect(()=>{		// is as didmount,willunmount create async function here
-		async function createGetOfflineData() {
-			const createTableOutput = await createTable();
-			let ownerData = {owner_name:'Anil singh patel', owner_mobile:'8349587093'};
-			const isetDataOutput = await insertData(ownerData);
-			const getDataOutput = await getData();
-		}
-		createGetOfflineData();
+		// async function createGetOfflineData() {
+		// 	const createTableOutput = await createTable();
+		// 	let ownerData = {owner_name:'Anil singh patel', owner_mobile:'8349587093'};
+		// 	const isertDataOutput = await insertData(ownerData);
+		// 	const getDataOutput = await getData();
+		// }
+		// createGetOfflineData();  // to check offline functionality
+		// crossPlatformToast(offlineSuccess);
 	}, []);
 
 	function onPressButton(){
+		// Functionality imaplementaion till now pending
+		console.log(`Functionality imaplementaion till now pending`);
+		crossPlatformToast(`Functionality imaplementaion till now pending`);
 	}
 
 
@@ -32,8 +42,16 @@ export const ProgramingPractiseRoot = (props)=>{
 				style={styles.pressableButtonStyle}
 				onPressIn={({nativeEvent})=> onPressButton()}
 			>
-				<Text style={styles.buttonTitle}>OpenOfflineDatabase</Text>
+				<Text style={styles.buttonTitle}>{offlineCheck}</Text>
 			</Pressable>
+
+			<Pressable
+				style={styles.pressableButtonStyle}
+				onPressIn={({nativeEvent})=> onPressButton()}
+			>
+				<Text style={styles.buttonTitle}>{checkContext}</Text>
+			</Pressable>
+			<ContextDataConsume/>
 		</View>
 	);
 }
@@ -41,8 +59,8 @@ export const ProgramingPractiseRoot = (props)=>{
 const styles = StyleSheet.create({
 	pressableButtonStyle:{
 		backgroundColor:'#175491',
-		width:80,
-		paddingVertical:5,
+		width:wp('80%'),
+		paddingVertical:15,
 		paddingHorizontal:5,
 		borderRadius:5,
 		marginTop:10,
