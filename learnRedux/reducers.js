acc4cd3f05c3bddaa85711ac704d5a7bcc63e5ad
AS-Patel <mca2020asp@gmail.com>
2023-09-07 16:21:46 +0530

    little bit work

--- learnRedux/reducers.js
+++ learnRedux/reducers.js
@@ -2,7 +2,7 @@ import { I18n } from 'i18n-js';
 import {translationValues} from '../staticDataFiles/translationValues';
 
 const transRef = new I18n(translationValues)
-transRef.locale = 'hi';	// By default set english
+transRef.locale = 'en';	// By default set english
 transRef.enableFallback = true;	// When a value is missing from a language it'll fall back to another language with the key present.
 // To see the fallback mechanism uncomment the line below to force the app to use the Hindi language.
 // i18n.locale = 'hi';
