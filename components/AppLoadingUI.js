f4ab99c3bca513856000c7848d5cd0c6b2d73f9b
AS-Patel <mca2020asp@gmail.com>
2023-09-25 06:45:05 +0530

    added the splash screen image and changed the apploading screen ui

--- components/AppLoadingUI.js
+++ components/AppLoadingUI.js
@@ -1,14 +1,18 @@
-import {Text, View, Pressable} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import {translationValues} from '../staticDataFiles/translationValues';
-const {hi:{appLoading, connectorApp, programingPractis}} = translationValues;
+const {en:{appLoading}} = translationValues;
 import {styles} from './componentStyles/AppLoadingUIStyles';
+import {CommonHeaderComponent} from '../components/commonHeaderComponent';
+
 
 
 export const AppLoadingUI = ()=>{
 	/* Used to show ui till the app is loading */
 	return(
 		<View style={styles.mainContainer}>
+			<CommonHeaderComponent/>
 			<Text style={styles.aploadingTextStyle}> {appLoading} </Text>
+			<Image source={require('../appImage/homeIcon.jpg')}  style={styles.loginIcon} />
 		</View>
 	);
 }
